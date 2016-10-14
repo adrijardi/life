01e8015a695696fbace3f4cb439d05cb1f038364
Adrian Lopez <adrijardi@gmail.com>
2016-10-14 07:32:22 +0100

    Improve SimulationManager test coverage

--- build.sbt
+++ build.sbt
@@ -8,3 +8,5 @@ libraryDependencies += "org.scalatest" % "scalatest_2.11" % "3.0.0" % "test"
 libraryDependencies += "org.scalafx" %% "scalafx" % "8.0.102-R11"
 libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.2.6"
 libraryDependencies += "org.scalaz" %% "scalaz-concurrent" % "7.2.6"
+
+coverageEnabled in Test := true
--- src/test/scala/com/coding42/gol/SimulationManagerSpec.scala
+++ src/test/scala/com/coding42/gol/SimulationManagerSpec.scala
@@ -1,17 +1,38 @@
 package com.coding42.gol
 
 import com.coding42.gol.SimulationManager.SimulationConfig
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.{FlatSpec, Matchers}
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Promise}
 import scala.concurrent.duration._
 
-class SimulationManagerSpec extends FlatSpec with Matchers {
+class SimulationManagerSpec extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
 
   it should "start and stop a simulation without failing" in {
     val generator = SimulationManager.generator( () => 100, _ => (), _ => ())
     val simulation = generator(SimulationConfig(None, 50, 10))
     simulation.start() shouldBe true
+    Await.result(simulation.stop(), 1.second)
+  }
+
+  it should "when a simulation is started the steps should get updated" in {
+    val promise = Promise[List[Int]]()
+    var steps: List[Int] = Nil
+
+    val stepUpdater: (Int) => Unit = step => {
+      steps = step :: steps
+
+      if(steps.length == 3) {
+          promise.success(steps)
+      }
+    }
+
+    val generator = SimulationManager.generator(() => 100, stepUpdater, _ => ())
+    val simulation = generator(SimulationConfig(None, 50, 10))
+    simulation.start() shouldBe true
+
+    promise.future.futureValue shouldBe List(3,2,1)
     Await.result(simulation.stop(), 1.second)
   }
 }
